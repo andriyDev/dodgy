b36b92337a90454fc8f2cb9ca6724e5af778a261
andriyDev <andriydzikh@gmail.com>
2023-02-19 15:22:47 -0800

    Port 3D linear program solver.
    I've made several modifications, but I've mostly added comments explaining the algorithm.

--- src/linear_programming.rs
+++ src/linear_programming.rs
@@ -3,6 +3,7 @@ use glam::Vec2;
 // A half-plane to act as a constraint on the linear program. This is
 // represented as a point and a direction, where the valid half-plane resides on
 // the "clockwise" side of `direction` and `point`.
+#[derive(Clone)]
 pub struct Line {
   pub point: Vec2,
   // Must always have length = 1
@@ -12,18 +13,18 @@ pub struct Line {
 // Determines the optimal value of the linear program defined be `lines` and
 // `preferred_value`. Optimality is defined as:
 //  1) The nearest point to `preferred_value` subject to:
-//      a) magnitude less than `max_length`
+//      a) magnitude less than `radius`
 //      b) in the intersection of the half-planes defined by `lines`.
 //  2) If the above does not exist, the half-planes are moved back at the same
 //      speed until a single point exists that satisfies all constraints.
 pub fn solve_linear_program(
   lines: &[Line],
   preferred_value: Vec2,
-  max_length: f32,
+  radius: f32,
 ) -> Vec2 {
   match solve_linear_program_2d(
     lines,
-    max_length,
+    radius,
     &OptimalValue::Point(preferred_value),
   ) {
     LinearProgram2DResult::Feasible(optimal_value) => optimal_value,
@@ -32,9 +33,10 @@ pub fn solve_linear_program(
       partial_value,
     } => solve_linear_program_3d(
       lines,
+      0,
+      radius,
       index_of_failed_line,
       partial_value,
-      max_length,
     ),
   }
 }
@@ -218,17 +220,120 @@ fn solve_linear_program_2d(
 
 // Solves the 3D linear program, after the 2D linear program was determined to
 // be infeasible. This effectively finds the first valid value when moving all
-// half-planes back at the same speed. `lines` are the half-plane constraints.
-// `index_of_failed_line` and `partial_value` are the results from the
-// infeasible 2D program. `max_length` limits the magnitude of the resulting
-// value.
+// non-rigid half-planes back at the same speed. `radius` limits the magnitude
+// of the resulting value. `rigid_constraint_count` determines the constraints
+// that will not be moved. These are assumed to be trivially satisfiable (in
+// practice these correspond to obstacles in RVO, which can be satisfied by a
+// velocity of 0). `index_of_failed_line` and `partial_value` are the results
+// from the infeasible 2D program, where `partial_value` is assumed to satisfy
+// all `constraints[0..index_of_failed_line]`.
 fn solve_linear_program_3d(
-  lines: &[Line],
+  constraints: &[Line],
+  rigid_constraint_count: usize,
+  radius: f32,
   index_of_failed_line: usize,
   partial_value: Vec2,
-  max_length: f32,
 ) -> Vec2 {
-  todo!()
+  debug_assert!(rigid_constraint_count < index_of_failed_line);
+
+  // The 2D linear program returned a partial value that is guaranteed to
+  // satisfy all constraints up to `index_of_failed_line`. So the current best
+  // value is `partial_value` and the deepest penetration into a constraint is 0
+  // (since all previous constraints are satisfied).
+  let mut penetration = 0.0;
+  let mut best_value = partial_value;
+
+  for (index, constraint) in
+    constraints[index_of_failed_line..].iter().enumerate()
+  {
+    if determinant(constraint.direction, constraint.point - best_value)
+      <= penetration
+    {
+      // `best_value` does not penetrate the constraint anymore than other
+      // constraints, so move on (this constraint will still be considered for
+      // future constraints).
+      continue;
+    }
+
+    let index = index + index_of_failed_line;
+
+    // The goal is to find the value that penetrates all constraints the least.
+    // While optimizing the value to penetrate `constraint` as little as
+    // possible (in the direction of the valid `constraint` half-plane), if the
+    // value is constrained such that all other constraints are not violated
+    // more than `constraint`, the resulting value will penetrate all
+    // constraints the least.
+
+    // Start a new problem to find the least penetrating value for `constraint`.
+    let mut penetration_constraints = Vec::with_capacity(index);
+    // Copy over all the rigid constraints - these must always be satisfied
+    // without modification.
+    penetration_constraints
+      .extend_from_slice(&constraints[0..rigid_constraint_count]);
+
+    for previous_constraint in &constraints[rigid_constraint_count..index] {
+      // The new constraint for `previous_constraint` is the half-plane such
+      // that `previous_constraint` is violated more than `constraint`. This
+      // half-plane is defined by the line through the intersection of both
+      // constraint lines (where both constraints are 0), and in the direction
+      // of equal violation. This direction is therefore the difference between
+      // `previous_constraint` and `constraint`.
+
+      let intersection_determinant =
+        determinant(constraint.direction, previous_constraint.direction);
+
+      let intersection_point;
+
+      if intersection_determinant.abs() <= RVO_EPSILON {
+        // The constraint lines are parallel.
+
+        if constraint.direction.dot(previous_constraint.direction) > 0.0 {
+          // The constraint lines point in the same direction, so optimizing
+          // `constraint` will also satisfy `previous_constraint` just as well.
+          continue;
+        }
+
+        // The constraint lines point in opposite directions, so the average of
+        // the two lines is where the constraints are violated the same amount.
+        intersection_point =
+          (constraint.point + previous_constraint.point) * 0.5;
+      } else {
+        // Use linear algebra to solve for the time of intersect between the
+        // constraint lines (for the `constraint` line).
+        let intersection_t = determinant(
+          previous_constraint.direction,
+          constraint.point - previous_constraint.point,
+        ) / intersection_determinant;
+
+        // Compute the actual intersection point.
+        intersection_point =
+          constraint.point + intersection_t * constraint.direction;
+      };
+
+      penetration_constraints.push(Line {
+        direction: (previous_constraint.direction - constraint.direction)
+          .normalize(),
+        point: intersection_point,
+      });
+    }
+
+    // This should in principle not happen. The optimal value is by definition
+    // already in the feasible region of the linear program. If it fails, it is
+    // due to small floating point errors, and the current `best_value` is kept.
+    if let LinearProgram2DResult::Feasible(result) = solve_linear_program_2d(
+      &penetration_constraints,
+      radius,
+      // The optimal value is the furthest value in the direction of the valid
+      // side of `constraint`'s half-plane.
+      &OptimalValue::Direction(constraint.direction.perp()),
+    ) {
+      best_value = result;
+      penetration =
+        determinant(constraint.direction, constraint.point - best_value);
+    }
+  }
+
+  best_value
 }
 
 fn determinant(a: Vec2, b: Vec2) -> f32 {
