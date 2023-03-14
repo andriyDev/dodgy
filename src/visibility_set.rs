7ec113d7150404bf3055e1a24c45dbafe72076d8
andriyDev <andriydzikh@gmail.com>
2023-03-14 00:29:56 -0700

    Write tests for visibility set, and fix revealed bugs.

--- src/visibility_set.rs
+++ src/visibility_set.rs
@@ -20,30 +20,33 @@ impl VisibilitySet {
     }
 
     fn is_cone_visible(set: &VisibilitySet, check_cone: Cone) -> bool {
-      if check_cone.left_ray.angle < set.cones[0].left_ray.angle {
-        return true;
-      }
-
-      if check_cone.right_ray.angle > set.cones.last().unwrap().right_ray.angle
-      {
-        return true;
-      }
-
-      for i in 0..(set.cones.len() - 1) {
+      for i in -1..set.cones.len() as isize {
         let free_cone = Cone {
           left_ray: ConeRay {
-            angle: set.cones[i].right_ray.angle,
+            angle: if i == -1 {
+              0.0
+            } else {
+              set.cones[i as usize].right_ray.angle
+            },
             distance: 0.0,
             point: Vec2::ZERO,
           },
           right_ray: ConeRay {
-            angle: set.cones[i + 1].left_ray.angle,
+            angle: if i == set.cones.len() as isize - 1 {
+              2.0 * PI
+            } else {
+              set.cones[(i + 1) as usize].left_ray.angle
+            },
             distance: 0.0,
             point: Vec2::ZERO,
           },
           line_id: 0,
           related_angle: 0.0,
         };
+
+        if free_cone.left_ray.angle == free_cone.right_ray.angle {
+          continue;
+        }
 
         if free_cone.overlaps_cone(check_cone) {
           return true;
@@ -76,7 +79,7 @@ impl VisibilitySet {
     match Cone::create_from_line(start_point, end_point, 0) {
       LineToCone::One(cone) => is_cone_visible(self, cone),
       LineToCone::Two(cone_1, cone_2) => {
-        is_cone_visible(self, cone_1) && is_cone_visible(self, cone_2)
+        is_cone_visible(self, cone_1) || is_cone_visible(self, cone_2)
       }
     }
   }
@@ -90,25 +93,24 @@ impl VisibilitySet {
       let mut visible = false;
 
       let mut old_cones = Vec::new();
-
       swap(&mut set.cones, &mut old_cones);
 
-      for i in -1..set.cones.len() as isize {
+      for i in -1..old_cones.len() as isize {
         let free_cone = Cone {
           left_ray: ConeRay {
             angle: if i == -1 {
               0.0
             } else {
-              set.cones[i as usize].right_ray.angle
+              old_cones[i as usize].right_ray.angle
             },
             distance: 0.0,
             point: Vec2::ZERO,
           },
           right_ray: ConeRay {
-            angle: if i == set.cones.len() as isize - 1 {
+            angle: if i == old_cones.len() as isize - 1 {
               2.0 * PI
             } else {
-              set.cones[i as usize + 1].left_ray.angle
+              old_cones[(i + 1) as usize].left_ray.angle
             },
             distance: 0.0,
             point: Vec2::ZERO,
@@ -223,7 +225,13 @@ impl VisibilitySet {
         }
       }
 
+      set.cones.sort_by(|a, b| a.left_ray.angle.total_cmp(&b.left_ray.angle));
+
       visible
+    }
+
+    if start_point == end_point {
+      return None;
     }
 
     let visible =
@@ -340,8 +348,8 @@ impl Cone {
   }
 
   fn overlaps_cone(&self, other: Cone) -> bool {
-    other.left_ray.angle <= self.right_ray.angle
-      && self.left_ray.angle <= other.right_ray.angle
+    other.left_ray.angle < self.right_ray.angle
+      && self.left_ray.angle < other.right_ray.angle
   }
 
   fn overlapping_cone(&self, other: Cone) -> Option<Cone> {
@@ -349,7 +357,7 @@ impl Cone {
       return None;
     }
 
-    let left_ray = if self.left_ray.angle <= other.left_ray.angle {
+    let left_ray = if self.left_ray.angle < other.left_ray.angle {
       // `self` contains `other.left_ray`.
       self.ray_at_angle(other.left_ray.angle)
     } else {
@@ -357,7 +365,7 @@ impl Cone {
       self.left_ray
     };
 
-    let right_ray = if other.right_ray.angle <= self.right_ray.angle {
+    let right_ray = if other.right_ray.angle < self.right_ray.angle {
       // `self` contains `other.right_ray`.
       self.ray_at_angle(other.right_ray.angle)
     } else {
@@ -384,5 +392,122 @@ impl ConeRay {
       distance: point.length(),
       point,
     }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use glam::Vec2;
+
+  use super::VisibilitySet;
+
+  #[test]
+  fn empty_always_visible() {
+    let viz = VisibilitySet::new();
+
+    assert!(viz.is_line_visible(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0)));
+    assert!(viz.is_line_visible(Vec2::new(-3.0, -2.0), Vec2::new(-3.001, -1.0)));
+    assert!(viz.is_line_visible(Vec2::new(100.0, 0.1), Vec2::new(-100.0, 0.1)));
+  }
+
+  #[test]
+  fn one_obstruction() {
+    let mut viz = VisibilitySet::new();
+    viz.add_line(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0));
+
+    assert!(!viz.is_line_visible(Vec2::new(2.0, 1.0), Vec2::new(1.0, 2.0)));
+    assert!(viz.is_line_visible(Vec2::new(1.0, 1.0), Vec2::new(-0.01, 2.0)));
+    assert!(viz.is_line_visible(Vec2::new(-1.0, 0.0), Vec2::new(0.0, -1.0)));
+  }
+
+  #[test]
+  fn two_connected_obstructions() {
+    let mut viz = VisibilitySet::new();
+    viz.add_line(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0));
+
+    assert!(viz.is_line_visible(Vec2::new(4.0, 1.0), Vec2::new(4.0, -1.0)));
+
+    viz.add_line(Vec2::new(3.0, 1.0), Vec2::new(3.0, -3.0));
+
+    assert!(!viz.is_line_visible(Vec2::new(4.0, 1.0), Vec2::new(4.0, -1.0)));
+    assert!(viz.is_line_visible(Vec2::new(1.0, 1.0), Vec2::new(-0.01, 2.0)));
+    assert!(viz.is_line_visible(Vec2::new(-1.0, 0.0), Vec2::new(0.0, -1.0)));
+  }
+
+  #[test]
+  fn two_disjoint_obstructions() {
+    let mut viz = VisibilitySet::new();
+    viz.add_line(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0));
+    viz.add_line(Vec2::new(-1.0, 0.0), Vec2::new(0.0, -1.0));
+
+    assert!(!viz.is_line_visible(Vec2::new(2.0, 1.0), Vec2::new(1.0, 2.0)));
+    assert!(!viz.is_line_visible(Vec2::new(-2.0, -1.0), Vec2::new(-1.0, -2.0)));
+    assert!(viz.is_line_visible(Vec2::new(-2.0, 1.0), Vec2::new(-1.0, 2.0)));
+    assert!(viz.is_line_visible(Vec2::new(2.0, -1.0), Vec2::new(1.0, -2.0)));
+  }
+
+  #[test]
+  fn touching_edges() {
+    let mut viz = VisibilitySet::new();
+    viz.add_line(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0));
+    viz.add_line(Vec2::new(-1.0, 0.0), Vec2::new(0.0, 1.0));
+
+    assert!(viz.is_line_visible(Vec2::new(-1.0, 0.5), Vec2::new(1.0, 0.5)));
+    assert!(!viz.is_line_visible(Vec2::new(-1.0, 1.5), Vec2::new(1.0, 1.5)));
+  }
+
+  macro_rules! assert_sets_eq {
+    ($actual: expr, { $($e:expr),* }) => {{
+      let mut actual_sorted = $actual.iter().copied().collect::<Vec<u32>>();
+      let mut expected_sorted = vec![$($e,)*];
+
+      actual_sorted.sort();
+      expected_sorted.sort();
+
+      assert_eq!(actual_sorted, expected_sorted);
+    }};
+  }
+
+  #[test]
+  fn layered_walls() {
+    let mut viz = VisibilitySet::new();
+    let line_0 = viz.add_line(Vec2::new(2.0, 0.0), Vec2::new(0.0, 2.0));
+    let line_1 = viz.add_line(Vec2::new(0.75, 0.25), Vec2::new(0.25, 0.75));
+    let line_2 = viz.add_line(Vec2::new(3.0, 1.0), Vec2::new(1.0, 3.0));
+
+    assert!(line_2.is_none());
+
+    assert_sets_eq!(viz.get_visible_line_ids(), {
+      line_0.unwrap(),
+      line_1.unwrap()
+    });
+  }
+
+  #[test]
+  fn obscured_wall_sharing_vertex() {
+    let mut viz = VisibilitySet::new();
+    let line_0 = viz.add_line(Vec2::new(1.0, 0.0), Vec2::new(0.0, 1.0));
+    // This wall is completely obscured.
+    viz.add_line(Vec2::new(1.0, 1.0), Vec2::new(0.0, 1.0));
+
+    assert_sets_eq!(viz.get_visible_line_ids(), { line_0.unwrap() });
+  }
+
+  #[test]
+  fn intersecting_obstructions() {
+    let mut viz = VisibilitySet::new();
+    let line_0 = viz.add_line(Vec2::new(1.0, 1.0), Vec2::new(-1.0, 1.0));
+    // This wall intersects the previous wall.
+    let line_1 = viz.add_line(Vec2::new(0.5, 0.9), Vec2::new(-0.5, 1.1));
+
+    assert_sets_eq!(viz.get_visible_line_ids(), { line_0.unwrap(), line_1.unwrap() });
+
+    // This line is in front of both obstructions.
+    assert!(viz.is_line_visible(Vec2::new(0.5, 0.75), Vec2::new(-0.5, 0.75)));
+    // This line is behind both obstructions.
+    assert!(!viz.is_line_visible(Vec2::new(0.5, 1.25), Vec2::new(-0.5, 1.25)));
+    // This line intersects the second obstruction, and will be in front of it
+    // and the first obstruction on the right.
+    assert!(viz.is_line_visible(Vec2::new(0.5, 0.95), Vec2::new(-0.5, 0.95)));
   }
 }
