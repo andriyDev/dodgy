aa314acf92715e4c100e6303ee6c9bea122b2ca8
andriyDev <andriydzikh@gmail.com>
2023-03-18 18:20:13 -0700

    Document the licensing and attributions stuff according to Apache v2.
    I hope I did this correctly...

--- src/linear_programming.rs
+++ src/linear_programming.rs
@@ -1,3 +1,24 @@
+// The contents of this file were primarily ported from Agent.cc from RVO2 with
+// significant alterations. As per the Apache-2.0 license, the original
+// copyright notice has been included, excluding those notices that do not
+// pertain to the derivate work:
+//
+// Agent.cc
+// RVO2 Library
+//
+// SPDX-FileCopyrightText: 2008 University of North Carolina at Chapel Hill
+//
+// The authors may be contacted via:
+//
+// Jur van den Berg, Stephen J. Guy, Jamie Snape, Ming C. Lin, Dinesh Manocha
+// Dept. of Computer Science
+// 201 S. Columbia St.
+// Frederick P. Brooks, Jr. Computer Science Bldg.
+// Chapel Hill, N.C. 27599-3175
+// United States of America
+//
+// <https://gamma.cs.unc.edu/RVO2/>
+
 use crate::determinant;
 use glam::Vec2;
 
