4165c1986cde9f294bc760a432b18b0b43e57b91
andriyDev <andriydzikh@gmail.com>
2023-04-01 14:09:23 -0700

    Add function comments for common functions and tests for the common mod.

--- src/common.rs
+++ src/common.rs
@@ -1,9 +1,15 @@
 use glam::Vec2;
 
+// Computes the 2D determinant of `a` and `b`, aka the 2D cross product.
 pub fn determinant(a: Vec2, b: Vec2) -> f32 {
   a.x * b.y - a.y * b.x
 }
 
+// Computes the "time" along both lines when the lines intersect. If the lines
+// are parallel, the result is None. The lines are not line segments; the time
+// is allowed to be any number, even negative or greater than one. The resulting
+// Vec2 contains the time for line 1 in the x component, and the time for line 2
+// in the y component.
 pub fn time_to_intersect_lines(
   line_1_start: Vec2,
   line_1_end: Vec2,
@@ -26,5 +32,53 @@ pub fn time_to_intersect_lines(
         determinant(relative_line_1_start, line_1_delta),
       ) / matrix_determinant,
     )
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use glam::Vec2;
+
+  use crate::common::{determinant, time_to_intersect_lines};
+
+  #[test]
+  fn determinant_correct() {
+    assert_eq!(determinant(Vec2::new(1.0, 2.0), Vec2::new(3.0, 4.0)), -2.0);
+  }
+
+  #[test]
+  fn intersecting_lines_get_correct_tti() {
+    assert_eq!(
+      time_to_intersect_lines(
+        Vec2::new(0.0, 0.0),
+        Vec2::new(4.0, 0.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(1.0, 3.0)
+      ),
+      Some(Vec2::new(0.25, -0.5))
+    );
+  }
+
+  #[test]
+  fn parallel_lines_get_none_tti() {
+    assert_eq!(
+      time_to_intersect_lines(
+        Vec2::new(0.0, 0.0),
+        Vec2::new(4.0, 0.0),
+        Vec2::new(1.0, 1.0),
+        Vec2::new(5.0, 1.0)
+      ),
+      None
+    );
+
+    assert_eq!(
+      time_to_intersect_lines(
+        Vec2::new(0.0, 0.0),
+        Vec2::new(4.0, 0.0),
+        Vec2::new(0.0, 0.0),
+        Vec2::new(4.0, 0.0)
+      ),
+      None
+    );
   }
 }
