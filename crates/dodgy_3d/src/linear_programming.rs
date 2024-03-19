928060bffdfcb742c7bf84e804ff180ab2c03a13
andriyDev <andriydzikh@gmail.com>
2024-03-19 00:53:18 -0700

    Implement linear program solver for plane constraints along a plane.
    This is similar to `solve_linear_program_along_line`.

--- crates/dodgy_3d/src/linear_programming.rs
+++ crates/dodgy_3d/src/linear_programming.rs
@@ -135,6 +135,106 @@ fn solve_linear_program_along_line(
   Ok(line.point + t * line.direction)
 }
 
+// Solves the linear program restricted to `plane`, and within the sphere
+// defined by `radius`. In addition, all `constraints` are used to further
+// restrict the resulting value. The best value is defined by `optimal_value`.
+fn solve_linear_program_along_plane(
+  plane: &Plane,
+  radius: f32,
+  constraints: &[Plane],
+  optimal_value: &OptimalValue,
+) -> Result<Vec3, ()> {
+  // We need to figure out the radius and center of the circle in `plane` that
+  // intersects the sphere.
+  let plane_distance_from_origin = plane.point.dot(plane.normal);
+  let plane_distance_squared_from_origin =
+    plane_distance_from_origin * plane_distance_from_origin;
+  let radius_squared = radius * radius;
+
+  if plane_distance_squared_from_origin > radius_squared {
+    // The plane is too far away from the origin, so no values are valid.
+    return Err(());
+  }
+
+  // Compute the radius and the center of the valid circle in `plane`.
+  let squared_radius_in_valid_plane =
+    radius_squared - plane_distance_squared_from_origin;
+  let valid_plane_center = plane_distance_from_origin * plane.normal;
+
+  let mut best_value = match optimal_value {
+    &OptimalValue::Direction(direction) => {
+      let projected_optimal_direction_in_plane =
+        direction - direction.dot(plane.normal) * plane.normal;
+      let squared_length_of_projection = projected_optimal_direction_in_plane
+        .dot(projected_optimal_direction_in_plane);
+
+      if squared_length_of_projection <= RVO_EPSILON {
+        valid_plane_center
+      } else {
+        valid_plane_center
+          + (squared_radius_in_valid_plane / squared_length_of_projection)
+            .sqrt()
+            * projected_optimal_direction_in_plane
+      }
+    }
+    &OptimalValue::Point(point) => {
+      let distance_from_plane = plane.signed_distance_to_plane(point);
+      let projected_point = point - distance_from_plane * plane.normal;
+
+      if projected_point.dot(projected_point) > radius_squared {
+        let relative_point = projected_point - valid_plane_center;
+        let squared_distance_from_center = relative_point.dot(relative_point);
+
+        valid_plane_center
+          + (squared_radius_in_valid_plane / squared_distance_from_center)
+            .sqrt()
+            * relative_point
+      } else {
+        projected_point
+      }
+    }
+  };
+
+  for (index, constraint) in constraints.iter().enumerate() {
+    let pen = constraint.signed_distance_to_plane(best_value);
+    if pen >= 0.0 {
+      // If the current best value is already on the valid side of the
+      // half-space defined by `constraint`, there is nothing to do.
+      continue;
+    }
+    let cross = constraint.normal.cross(plane.normal);
+    if cross.dot(cross) <= RVO_EPSILON {
+      // `plane` is parallel to `constraint`, but the current best value (which
+      // satisfies `plane`) is on the wrong side of `constraint`. Therefore, we
+      // can't satisfy both `plane` and `constraint`.
+      return Err(());
+    }
+
+    let cross = cross.normalize();
+    let line_normal = cross.cross(plane.normal);
+    let line = Line {
+      direction: cross,
+      point: plane.point
+        - constraint.signed_distance_to_plane(plane.point)
+          / line_normal.dot(constraint.normal)
+          * line_normal,
+    };
+
+    let Ok(new_value) = solve_linear_program_along_line(
+      &line,
+      radius,
+      &constraints[0..index],
+      optimal_value,
+    ) else {
+      return Err(());
+    };
+
+    best_value = new_value;
+  }
+
+  Ok(best_value)
+}
+
 #[cfg(test)]
 mod tests {
   use super::*;
@@ -365,6 +465,181 @@ mod tests {
           1.0,
           &infeasible_constraint,
           &OptimalValue::Direction(Vec3::X)
+        ),
+        Err(())
+      );
+    }
+  }
+
+  mod solve_linear_program_along_plane_tests {
+    use glam::Vec3;
+
+    use crate::linear_programming::{
+      solve_linear_program_along_plane, OptimalValue,
+    };
+
+    use super::Plane;
+
+    #[test]
+    fn uses_projected_optimal_point() {
+      let value_plane = Plane {
+        point: Vec3::new(2.0, 2.0, 2.0),
+        normal: Vec3::new(1.0, 0.0, 1.0).normalize(),
+      };
+
+      assert_vec3_near!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          10.0,
+          &[],
+          &OptimalValue::Point(Vec3::new(0.0, 1.0, 0.0)),
+        )
+        .unwrap(),
+        Vec3::new(2.0, 1.0, 2.0)
+      );
+
+      assert_vec3_near!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          10.0,
+          &[],
+          &OptimalValue::Point(Vec3::new(0.0, 15.0, 0.0)),
+        )
+        .unwrap(),
+        // The radius of the sphere around the origin is 10.0. The distance of
+        // the plane from the origin is 2*sqrt(2), so the radius of the
+        // intersection is sqrt(92).
+        Vec3::new(2.0, 92f32.sqrt(), 2.0)
+      );
+    }
+
+    #[test]
+    fn uses_projected_optimal_direction() {
+      let value_plane = Plane {
+        // This is equivalent to the point (2.0, 0.0, 2.0).
+        point: Vec3::new(-10.0, 10.0, 14.0),
+        normal: Vec3::new(1.0, 0.0, 1.0).normalize(),
+      };
+
+      assert_vec3_near!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          10.0,
+          &[],
+          &OptimalValue::Direction(Vec3::new(0.0, 1.0, 0.0)),
+        )
+        .unwrap(),
+        // The radius of the sphere around the origin is 10.0. The distance of
+        // the plane from the origin is 2*sqrt(2), so the radius of the
+        // intersection is sqrt(92).
+        Vec3::new(2.0, 92f32.sqrt(), 2.0)
+      );
+
+      // The direction is perpendicular to the surface, so we just pick the
+      // center of the valid circle along the plane.
+      assert_vec3_near!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          10.0,
+          &[],
+          &OptimalValue::Direction(Vec3::new(1.0, 0.0, 1.0).normalize()),
+        )
+        .unwrap(),
+        Vec3::new(2.0, 0.0, 2.0)
+      );
+    }
+
+    #[test]
+    fn satisfies_constraints() {
+      let value_plane =
+        Plane { point: Vec3::ZERO, normal: Vec3::new(0.0, 0.0, 1.0) };
+
+      let constraints = [
+        Plane {
+          point: Vec3::new(0.0, -0.1, 0.0),
+          normal: Vec3::new(0.0, 1.0, 0.0),
+        },
+        Plane {
+          point: Vec3::new(0.0, 0.5, 0.0),
+          normal: Vec3::new(-1.0, -1.0, 0.0),
+        },
+      ];
+
+      assert_vec3_near!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          1.0,
+          &constraints,
+          &OptimalValue::Direction(Vec3::new(1.0, 0.0, 0.0))
+        )
+        .unwrap(),
+        Vec3::new(0.6, -0.1, 0.0)
+      );
+    }
+
+    #[test]
+    fn constraints_are_infeasible() {
+      let value_plane =
+        Plane { point: Vec3::ZERO, normal: Vec3::new(0.0, 0.0, 1.0) };
+
+      let constraints = [
+        Plane {
+          point: Vec3::new(0.0, -0.1, 0.0),
+          normal: Vec3::new(0.0, -1.0, 0.0),
+        },
+        Plane {
+          point: Vec3::new(0.0, 0.1, 0.0),
+          normal: Vec3::new(0.0, 1.0, 0.0),
+        },
+      ];
+
+      assert_eq!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          1.0,
+          &constraints,
+          &OptimalValue::Point(Vec3::ZERO)
+        ),
+        Err(())
+      );
+    }
+
+    #[test]
+    fn value_plane_and_constraint_are_parallel() {
+      let value_plane =
+        Plane { point: Vec3::ZERO, normal: Vec3::new(0.0, 0.0, 1.0) };
+
+      // This constraint invalidates the whole value plane.
+      let constraints = [Plane {
+        point: Vec3::new(0.0, 0.0, 0.1),
+        normal: Vec3::new(0.0, 0.0, 1.0),
+      }];
+
+      assert_eq!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          1.0,
+          &constraints,
+          &OptimalValue::Point(Vec3::ZERO)
+        ),
+        Err(())
+      );
+    }
+
+    #[test]
+    fn value_plane_outside_radius() {
+      // All these values have a distance larger than 1.0.
+      let value_plane = Plane {
+        point: Vec3::new(0.0, 0.0, 1.1),
+        normal: Vec3::new(0.0, 0.0, 1.0),
+      };
+
+      assert_eq!(
+        solve_linear_program_along_plane(
+          &value_plane,
+          1.0,
+          &[],
+          &OptimalValue::Point(Vec3::ZERO)
         ),
         Err(())
       );
