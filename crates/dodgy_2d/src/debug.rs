061c03c25a1b7f1c0f23a881b9565a38df70c912
andriyDev <andriydzikh@gmail.com>
2024-12-14 23:23:06 -0800

    Add a `debug` feature to both crates to allow access to the internal data without needing to fork everything.

---
+++ crates/dodgy_2d/src/debug.rs
@@ -0,0 +1,22 @@
+// Re-export Line so we can use it to provide debug data.
+pub use crate::linear_programming::Line;
+
+/// Internal data that is used to generate the final suggested velocity.
+pub enum DebugData {
+  /// The original problem (where the agent uses its current velocity) was
+  /// solved.
+  Satisfied {
+    /// The constraints that needed to be satisfied.
+    constraints: Vec<Line>,
+  },
+  /// The original problem (where the agent uses its current velocity) was
+  /// invalid, so the algorithm fell back to pretending the agent has a
+  /// zero-velocity, which is trivially satisfiable.
+  Fallback {
+    /// The constraints for the original problem.
+    original_constraints: Vec<Line>,
+    /// The constraints after falling back (pretending the agent has zero
+    /// velocity).
+    fallback_constraints: Vec<Line>,
+  },
+}
